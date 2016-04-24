0f08116eed1a3befcd6228eb0d3868b12486a063
Yuen-rMBP <rxg9527@sina.cn>
2016-04-24 16:21:20 +0800

    0424 简单使用 AFNetworking

--- AFNetworking/ViewController.m
+++ AFNetworking/ViewController.m
@@ -27,12 +27,30 @@
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request
                                             completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
                                                 dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-                                                    NSString *dataStr = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-                                                    NSLog(@"\n%@", dataStr);
+//                                                    NSString *dataStr = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+//                                                    NSLog(@"\n%@", dataStr);
                                                 });
                                             }];
     [task resume];
 
+    /**
+     *  简单使用 AFNetworking
+     */
+    AFHTTPSessionManager *manager = [[AFHTTPSessionManager alloc] initWithBaseURL:[NSURL URLWithString:@"http://www.peikua.com/"]];
+    manager.responseSerializer.acceptableContentTypes = [manager.responseSerializer.acceptableContentTypes setByAddingObject:@"text/html"];
+    [manager GET:@"v1/ad/getHomeNav"
+      parameters:nil
+        progress:nil
+         success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                 NSLog(@"\n%@", responseObject);
+             });
+         }
+         failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                 NSLog(@"\n%@", error);
+             });
+         }];
 }
 
 - (void)didReceiveMemoryWarning {
