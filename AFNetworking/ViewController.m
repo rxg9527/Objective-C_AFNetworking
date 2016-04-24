76d0984563259d978f63c6af22964d1157bb7703
Yuen-rMBP <rxg9527@sina.cn>
2016-04-24 15:41:52 +0800

    0424 简单 URLSession 使用示例

--- AFNetworking/ViewController.m
+++ AFNetworking/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    /**
+     *  简单 URLSession 使用示例
+     */
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://www.peikua.com/v1/ad/getHomeNav"]];
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request
+                                            completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+                                                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                                                    NSString *dataStr = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+                                                    NSLog(@"\n%@", dataStr);
+                                                });
+                                            }];
+    [task resume];
+
 }
 
 - (void)didReceiveMemoryWarning {
